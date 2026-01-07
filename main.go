b48df900e00e15cabd30c6ec5d2f242c06124115
mooss <mooss@protonmail.com>
2026-01-07 20:08:47 +0100

    Handle the case where comment creation time is an int

--- main.go
+++ main.go
@@ -26,6 +26,37 @@ type RedditComment struct {
 	CreatedUTC string `json:"created_utc"`
 }
 
+// UnmarshalJSON is a custom unmarshaller handling the fact that CreatedUTC is sometimes a string
+// and sometimes a number.
+func (c *RedditComment) UnmarshalJSON(data []byte) error {
+	type Alias RedditComment
+	temp := &struct {
+		CreatedUTC any `json:"created_utc"`
+		*Alias
+	}{
+		Alias: (*Alias)(c),
+	}
+
+	if err := json.Unmarshal(data, &temp); err != nil {
+		return err
+	}
+
+	switch v := temp.CreatedUTC.(type) {
+	case string:
+		c.CreatedUTC = v
+	case float64:
+		c.CreatedUTC = fmt.Sprintf("%.0f", v)
+	case int:
+		c.CreatedUTC = fmt.Sprintf("%d", v)
+	case nil:
+		c.CreatedUTC = ""
+	default: // At worse, try to coerce to string.
+		c.CreatedUTC = fmt.Sprintf("%v", v)
+	}
+
+	return nil
+}
+
 func truncate(s string, maxLen int) string {
 	if len(s) <= maxLen {
 		return s
